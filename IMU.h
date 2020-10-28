2e27e8e7b5f84ea0cafde81c0465c0e7548d1904
Matt <maki2275@colorado.edu>
2020-10-28 12:41:19 -0600

    Initial driver files

---
+++ IMU.h
@@ -0,0 +1,15 @@
+/*
+ * IMU.h
+ *
+ *  Created on: Oct 28, 2020
+ *      Author: Matt
+ */
+
+#ifndef IMU_H_
+#define IMU_H_
+
+
+
+
+
+#endif /* IMU_H_ */
---
+++ ultrasonic.h
@@ -0,0 +1,15 @@
+/*
+ * ultrasonic.h
+ *
+ *  Created on: Oct 28, 2020
+ *      Author: Matt
+ */
+
+#ifndef ULTRASONIC_H_
+#define ULTRASONIC_H_
+
+void config_reflectance(void);
+
+void followPath(void);
+
+#endif /* ULTRASONIC_H_ */
