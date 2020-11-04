3f541f40b6eaa2101ec999cb95012c4dbb12b578
Matt <maki2275@colorado.edu>
2020-11-04 15:03:42 -0700

    Added new driver files (Clock/Reflectance) and .gitignore

--- IMU.h
+++ IMU.h
@@ -8,8 +8,6 @@
 #ifndef IMU_H_
 #define IMU_H_
 
-
-
-
+void config_IMU(void);
 
 #endif /* IMU_H_ */
--- main.c
+++ main.c
@@ -1,10 +1,31 @@
 #include "msp.h"
-
+#include "stdio.h"
+#include "reflectanceArray.h"
+#include "Clock.h"
 
 /**
  * main.c
  */
+
+void Initialize();
+
 void main(void)
 {
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+    Initialize();
+
+	uint8_t data, light_data;
+	while(1)
+	{
+	    data = Reflectance_Read(1000);
+	    light_data = LIGHT_BAR(data, 0);
+	    printf("Data: %i \n LightData: \i", data, light_data);
+	}
+}
+
+void Initialize()
+{
+    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     // stop watchdog timer
+    Clock_Init48MHz();
+
+    Reflectance_Init();
 }
