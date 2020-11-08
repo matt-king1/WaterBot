960fbf34155efa05767fa0f6d949d58f3982d72e
Matt <maki2275@colorado.edu>
2020-11-07 17:51:01 -0700

    Project now compiles

--- servo.c
+++ servo.c
@@ -5,7 +5,7 @@
 
 void forward(uint8_t stop_condition) {
     uint8_t data; uint8_t dis;
-    data = Reflectance_read(1000);
+    data = Reflectance_Read(1000);
     while (data != stop_condition) {
         dis = Reflectance_Position(data);
         if (dis > 5) {
@@ -16,22 +16,22 @@ void forward(uint8_t stop_condition) {
                 move_left();
             }
         }
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write(); //fill in these functions
-        data = Reflectance_read(1000);
+        servo_write(0, 45);
+        servo_write(1, 45);
+        servo_write(2, 45);
+        servo_write(3, 45);
+        servo_write(4, 45);
+        servo_write(5, 45);
+        servo_write(6, 45);
+        servo_write(7, 45); //fill in these functions
+        data = Reflectance_Read(1000);
     }
     return;
 }
 
 void backward(uint8_t stop_condition) {
     uint8_t data; uint8_t dis;
-    data = Reflectance_read(1000);
+    data = Reflectance_Read(1000);
     while (data != stop_condition) { //checks if reflectance is equal to what would signify plant
         dis = Reflectance_Position(data);
         if (dis > 5) { //checks if straight
@@ -42,14 +42,14 @@ void backward(uint8_t stop_condition) {
                 move_left();
             } // moves direction needed to be straight
         }
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write(); //fill in these functions
+        servo_write(0, 45);
+        servo_write(1, 45);
+        servo_write(2, 45);
+        servo_write(3, 45);
+        servo_write(4, 45);
+        servo_write(5, 45);
+        servo_write(6, 45);
+        servo_write(7, 45); //fill in these functions
         data = Reflectance_Read(1000);
     }
     return;
@@ -58,17 +58,17 @@ void backward(uint8_t stop_condition) {
 bool leftCheck() {
     uint8_t init; uint8_t fin;
     init = Reflectance_Read(1000);
-    init = Reflectance_Position(init)
-    servo_write();
-    servo_write();
-    servo_write();
-    servo_write();
-    servo_write();
-    servo_write();
-    servo_write();
-    servo_write(); //fill in these functions
+    init = Reflectance_Position(init);
+    servo_write(0, 45);
+    servo_write(1, 45);
+    servo_write(2, 45);
+    servo_write(3, 45);
+    servo_write(4, 45);
+    servo_write(5, 45);
+    servo_write(6, 45);
+    servo_write(7, 45); //fill in these functions
     fin = Reflectance_Read(1000);
-    fin = Reflecatance_Read(fin)
+    fin = Reflectance_Read(fin);
     if (fin > init) {
         return false;
     }
@@ -83,14 +83,14 @@ void move_right(void) {
     uint8_t dis;
     dis = Reflectance_Position(Reflectance_Read(1000));
     while (dis > 3) {
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write(); //fill in these functions
+        servo_write(0, 45);
+        servo_write(1, 45);
+        servo_write(2, 45);
+        servo_write(3, 45);
+        servo_write(4, 45);
+        servo_write(5, 45);
+        servo_write(6, 45);
+        servo_write(7, 45); //fill in these functions
         dis = Reflectance_Position(Reflectance_Read(1000));
     }
     return;
@@ -100,14 +100,14 @@ void move_left(void) {
     uint8_t dis;
     dis = Reflectance_Position(Reflectance_Read(1000));
     while (dis > 3) {
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write();
-        servo_write(); //fill in these functions
+        servo_write(0, 45);
+        servo_write(1, 45);
+        servo_write(2, 45);
+        servo_write(3, 45);
+        servo_write(4, 45);
+        servo_write(5, 45);
+        servo_write(6, 45);
+        servo_write(7, 45); //fill in these functions
         dis = Reflectance_Position(Reflectance_Read(1000));
     }
     return;
